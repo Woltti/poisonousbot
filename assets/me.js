60df197d50f96a3eba02df4c8ec76d27404858be
Woltti <37505452+Woltti@users.noreply.github.com>
2018-12-25 10:52:11 +0200

    ddd

--- assets/me.js
+++ assets/me.js
@@ -16,10 +16,27 @@ function loaded(){
 
 var x = 0;
 
-var titleText = [ "ur", "edgy", "moving","title","here"];
+<script type="text/javascript">
+                var msg  = document.title ="Edgy Title";
+                var speed = 150;
+                var endChar = " ";
+                var pos = 0;
+                
+                function moveTitle()
+                {
+                     var ml = msg.length;
+                        
+                    title = msg.substr(pos,ml) + endChar + msg.substr(0,pos);
+                  document.title = title;
+                    
+                  pos++;
+                  if (pos > ml) pos=0;
+                  window.setTimeout("moveTitle()",speed);
+                }
+            
+                moveTitle();
+            </script>
 
-function loop(){
 
-	document.getElementsByTagName("title")[0].innerHTML = titleText[x++%titleText.length];
 
 }
